33e4aeb95100dfc202a936e00a5c89b280588674
javed001999 <javedansari001999@gmail.com>
2023-07-14 00:35:26 +0530

    added multiply.c

---
+++ multiply.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a=10,b=20,mul;
+    mul=a*b;
+    printf("multiply of 2 number is : %d",mul);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
