4d19948a2ce6bc8803178c7e4527dc7d659f9059
javed001999 <javedansari001999@gmail.com>
2023-07-14 01:23:30 +0530

    added implementation for diff.c

---
+++ diff.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a=20,b=10,mul;
+    mul=a-b;
+    printf("Subtraction of 2 number is : %d",mul);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
